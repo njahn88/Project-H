cffad9c21a212513f86ef2fe772a7c0787e501b6
njahn88 <nja010@email.latech.edu>
2023-11-17 11:09:12 -0600

    Added main menu scene, main menu manager, buttons, cycle between buttons

---
+++ Project H/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] _menuButtons;
+
+    private int _menuButtonsIndex = 0;
+    private void OnEnable()
+    {
+        InputManager.OnMovementInput += SwitchSelectedButton;
+    }
+
+    private void Start()
+    {
+        _menuButtons[_menuButtonsIndex].Select();
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnMovementInput -= SwitchSelectedButton;
+    }
+
+    //Cycles through the avaiable buttons on the main menu, updating current index
+    private void SwitchSelectedButton(Vector2 moveDirection)
+    {
+        if(moveDirection.y > 0)
+        {
+            if(_menuButtonsIndex == 0) _menuButtonsIndex = _menuButtons.Length - 1;
+            else _menuButtonsIndex--;
+        }
+        else
+        {
+            if (_menuButtonsIndex == _menuButtons.Length - 1) _menuButtonsIndex = 0;
+            else _menuButtonsIndex++;
+        }
+        _menuButtons[_menuButtonsIndex].Select();
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
